00e974cd6735256f86a1cbed22926093411008c8
Drew Gregory <djgregny@gmail.com>
2025-11-07 22:14:41 -0800

    update lockfile

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -11,7 +11,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
-import { GiSpeechBubbles } from "react-icons/gi";
+import { GiBubbles } from "react-icons/gi";
 import NavContainer from "../components/NavContainer";
 import { useTeam } from "../components/useTeam";
 import { getTeamFromCookie } from "../lib/team";
@@ -143,7 +143,7 @@ export default function Page({
       <VStack spacing={2} alignItems="stretch" pb="0">
         {messages.length === 0 ? (
           <Flex direction="column" alignItems="center" justifyContent="center" py={10} color="gray.400">
-            <Icon as={GiSpeechBubbles} boxSize={20} mb={4} />
+            <Icon as={GiBubbles} boxSize={20} mb={4} />
             <Text fontSize="lg">No messages yet. Start the conversation!</Text>
           </Flex>
         ) : (
